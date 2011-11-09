442e51e7a54362c5cf717fd43b557425494ce957
vitorzachi@gmail.com <vitorzachi@gmail.com@2cfce62b-586e-e36e-38e3-0103c186e568>
2011-11-09 20:54:38 +0000

    troca de diretorio

--- appNormal/src/br/edu/unoesc/petshop/util/ListaUrl.java
+++ appNormal/src/br/edu/unoesc/petshop/util/ListaUrl.java
@@ -2,16 +2,16 @@ package br.edu.unoesc.petshop.util;
 
 public class ListaUrl {
 
-	public static final String URL_ANIMAL = "animal.xhtml";
-	public static final String URL_ATENDIMENTO = "atendimento.xhtml";
-	public static final String URL_ATIVIDADE = "atividade.xhtml";
-	public static final String URL_CEP = "cep.xhtml";
-	public static final String URL_CIDADE = "cidade.xhtml";
-	public static final String URL_CLIENTE = "cliente.xhtml";
-	public static final String URL_EMPRESA = "empresa.xhtml";
-	public static final String URL_TIPO_ANIMAL = "tipoAnimal.xhtml";
-	public static final String URL_UNIDADE_FEDERATIVA = "unidadeFederativa.xhtml";
-	public static final String URL_USUARIO = "usuario.xhtml";
+	public static final String URL_ANIMAL = "/paginas/animal.xhtml";
+	public static final String URL_ATENDIMENTO = "/paginas/atendimento.xhtml";
+	public static final String URL_ATIVIDADE = "/paginas/atividade.xhtml";
+	public static final String URL_CEP = "/paginas/cep.xhtml";
+	public static final String URL_CIDADE = "/paginas/cidade.xhtml";
+	public static final String URL_CLIENTE = "/paginas/cliente.xhtml";
+	public static final String URL_EMPRESA = "/paginas/empresa.xhtml";
+	public static final String URL_TIPO_ANIMAL = "/paginas/tipoAnimal.xhtml";
+	public static final String URL_UNIDADE_FEDERATIVA = "/paginas/unidadeFederativa.xhtml";
+	public static final String URL_USUARIO = "/paginas/usuario.xhtml";
 	public static final String URL_HOME = "homeConteudo.xhtml";
 
 }
